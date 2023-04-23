20a3902935a90f36d53734f342173a2b45994ba1
Ethan Martin <ethan@emar10.dev>
2023-04-22 21:07:24 -0400

    Create initial Containerfile

---
+++ Containerfile
@@ -0,0 +1,38 @@
+# Rust Dedicated Server
+
+FROM docker.io/steamcmd/steamcmd:ubuntu
+LABEL maintainer="ethan@emar10.dev"
+
+# Volumes
+RUN mkdir /gamedata
+VOLUME [ "/gamedata" ]
+
+# Environment Variables
+ENV RUSTDS_SERVER_PORT="28015"
+ENV RUSTDS_SERVER_LEVEL="Procedural Map"
+ENV RUSTDS_SERVER_SEED="1234"
+ENV RUSTDS_SERVER_WORLDSIZE="4000"
+ENV RUSTDS_SERVER_MAXPLAYERS="10"
+ENV RUSTDS_SERVER_HOSTNAME="Rust Dedicated Server"
+ENV RUSTDS_SERVER_DESCRIPTION="Containerized Rust Dedicated Server"
+ENV RUSTDS_SERVER_URL="https://example.com"
+ENV RUSTDS_SERVER_HEADERIMAGE=""
+ENV RUSTDS_SERVER_IDENTITY="rustds"
+ENV RUSTDS_SERVER_QUERYPORT="28017"
+ENV RUSTDS_RCON_PORT="28016"
+ENV RUSTDS_RCON_PASSWORD="youshouldchangethis"
+ENV RUSTDS_RCON_WEB="1"
+ENV RUSTDS_APP_PORT="28082"
+ENV ENABLE_EAC="1"
+
+# Ports
+EXPOSE ${RUSTDS_SERVER_PORT}/udp
+EXPOSE ${RUSTDS_SERVER_QUERYPORT}/tcp
+EXPOSE ${RUSTDS_RCON_PORT}/tcp
+EXPOSE ${RUSTDS_APP_PORT}/tcp
+
+# Copy Entrypoint Script
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+echo Installing/Updating Rust Dedicated Server...
+steamcmd +@ShutdownOnFailedCommand 1 \
+         +force_install_dir /gamedata \
+         +login anonymous \
+         +app_update 258550 validate \
+         +exit
+
+echo Launching server...
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gamedata/RustDedicated_Data/Plugins/x86_64:/gamedata/RustDedicated_Data/Plugins
+pushd /gamedata
+./RustDedicated -batchmode \
+                +server.port "${RUSTDS_SERVER_PORT}" \
+                +server.level "${RUSTDS_SERVER_LEVEL}" \
+                +server.seed "${RUSTDS_SERVER_SEED}" \
+                +server.worldsize "${RUSTDS_SERVER_WORLDSIZE}" \
+                +server.maxplayers "${RUSTDS_SERVER_MAXPLAYERS}" \
+                +server.hostname "${RUSTDS_SERVER_HOSTNAME}" \
+                +server.description "${RUSTDS_SERVER_DESCRIPTION}" \
+                +server.url "${RUSTDS_SERVER_URL}" \
+                +server.headerimage "${RUSTDS_SERVER_HEADERIMAGE}" \
+                +server.identity "${RUSTDS_SERVER_IDENTITY}" \
+                +server.queryport "${RUSTDS_SERVER_QUERYPORT}" \
+                +rcon.port "${RUSTDS_RCON_PORT}" \
+                +rcon.password "${RUSTDS_RCON_PASSWORD}" \
+                +rcon.web "${RUSTDS_RCON_WEB}" \
+                +app.port "${RUSTDS_APP_PORT}" \
+                +server.secure "${ENABLE_EAC}" \
+
